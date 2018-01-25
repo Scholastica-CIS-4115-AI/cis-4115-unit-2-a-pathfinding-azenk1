b634604952b073e10f86ee5fa41859433086a4ab
azenk <azenk@labteag17-1WQY.storms.css.edu>
2018-01-25 13:20:57 -0600

    Made necessary changes. Finds shortest paths.

--- src/aStar/AStar.java
+++ src/aStar/AStar.java
@@ -127,11 +127,11 @@ public class AStar {
             frontierList.add(neighbor);
             neighbor.setPreviousNode(current);
             neighbor.distanceFromStart = newDistanceFromStart;
-            //neighbor.heuristicDistanceToGoal = getEstimatedDistanceToGoal(neighbor.x, neighbor.y, map.getGoalLocation().x, map.getGoalLocation().y);
-            //neighbor.TotalDistanceFromGoal = neighbor.distanceFromStart + neighbor.heuristicDistanceToGoal;
-            neighbor.TotalDistance = neighbor.distanceFromStart;
+            neighbor.heuristicDistanceToGoal = calcEstimatedDistance(neighbor.x, neighbor.y, map.getGoalLocation().x, map.getGoalLocation().y);
+            neighbor.TotalDistance = neighbor.distanceFromStart + neighbor.heuristicDistanceToGoal;
+            //neighbor.TotalDistance = neighbor.distanceFromStart;
             //keep the frontier list sorted so you explore the best nodes first
-            //Collections.sort(frontierList);
+            Collections.sort(frontierList);
         } else {
             System.out.println("Neighbor already on Fronter list... do nothing?");
             // If the node is already on the frontier, do we need to do anything?  
